6fd043637874c49ad0a86ea916ea5ffc1e561fa3
zafarzaeem12 <106338772+zafarzaeem12@users.noreply.github.com>
2023-08-19 02:15:49 +0500

    complete create api of logs

--- controllers/api/logsController.js
+++ controllers/api/logsController.js
@@ -2,51 +2,27 @@ const Logs = require('../../models/Logs');
 const moment = require('moment')
 const create_meeting_schedule = async (req,res,next) => {
   try {
-    if(Data.set_remainder === true) {
-        let Data = {
-            category_id : req.body.category_id,
-            title : req.body.title,
-            description : req.body.description,
-            start_time : moment(req.body.start_time).format("YYYY-MM-DDThh:mm A"),
-            end_time :  moment(req.body.end_time).format("YYYY-MM-DDThh:mm A"),
-            date :  moment(req.body.date).format("YYYY-MM-DD"),
-            location : req.body.location,
-            set_remainder : req.body.set_remainder,
-            start_remainder_time: moment(req.body.start_remainder_time).format("YYYY-MM-DDThh:mm A"),
-            end_remainder_time: moment(req.body.end_remainder_time).format("YYYY-MM-DDThh:mm A"),
-            recording : req.file ? req.file.path.replace(/\\/g, "/") : req.body.recording,
-        } 
-
-        const create_meeting = await Logs.create(Data);
-        return res
-    .status(200)
-    .send({ 
-        message : "Meeting Created Successfully",
-        data : create_meeting
-    })
-    }
-        else if (Data.set_remainder === false ) {
-            let Data = {
-                category_id : req.body.category_id,
-                title : req.body.title,
-                description : req.body.description,
-                start_time : moment(req.body.start_time).format("YYYY-MM-DDThh:mm A"),
-                end_time :  moment(req.body.end_time).format("YYYY-MM-DDThh:mm A"),
-                date :  moment(req.body.date).format("YYYY-MM-DD"),
-                location : req.body.location,
-                set_remainder : req.body.set_remainder,
-                recording : req.file ? req.file.path.replace(/\\/g, "/") : req.body.recording,
-            }
-
-            const create_meeting = await Logs.create(Data);
-            return res
-        .status(200)
-        .send({ 
-            message : "Meeting Created Successfully",
-            data : create_meeting
-        })
-        }
-
+    let Data = {
+        category_id : req.body.category_id,
+        title : req.body.title,
+        description : req.body.description,
+        start_time : moment(req.body.start_time).format("YYYY-MM-DDThh:mm A"),
+        end_time :  moment(req.body.end_time).format("YYYY-MM-DDThh:mm A"),
+        date :  moment(req.body.date).format("YYYY-MM-DD"),
+        location : req.body.location,
+        set_remainder : JSON.parse(req.body.set_remainder),
+        start_remainder_time: JSON.parse(req.body.set_remainder) === true ? moment(req.body.start_remainder_time).format("YYYY-MM-DDThh:mm A") : null,
+        end_remainder_time: JSON.parse(req.body.set_remainder) === true ?  moment(req.body.end_remainder_time).format("YYYY-MM-DDThh:mm A") : null,
+        recording : req.file ? req.file.path.replace(/\\/g, "/") : req.body.recording,
+    } 
+    
+    const create_meeting = await Logs.create(Data);
+    return res
+.status(200)
+.send({ 
+    message : "Meeting Created Successfully",
+    data : create_meeting
+})
     
    
     
